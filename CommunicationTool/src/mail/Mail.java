951885a50c03500d7c3c3a7ca3b919f5eea11bf7
Ramin012 <76204841+Ramin012@users.noreply.github.com>
2021-04-18 01:35:03 +0200

    .

--- CommunicationTool/src/mail/Mail.java
+++ CommunicationTool/src/mail/Mail.java
@@ -20,7 +20,9 @@ public class Mail {
     
        private static ArrayList<String> emailList = new ArrayList<String>(); 
        private static ArrayList<String> nameList = new ArrayList<String>();
-         
+       private static ArrayList<String> commentList = new ArrayList<String>();      
+       private static String creatorOfPost;
+       private static String emailOfCreator;
     
     public void lagraEmail() {
         
@@ -82,6 +84,80 @@ public class Mail {
             System.out.println(nameList.get(i));
             
         }
+        
+    }
+    
+    public void commentsAndPost(int postID) {
+        
+        ResultSet userInfo1 = null;
+        ResultSet userInfo2 = null;
+        
+        try {
+                Statement st = Connectivity.ConnectionClass.conn.createStatement();
+                System.out.println(postID);
+                //fånga skapare av tråden
+                String creatorPost = "select Användare.Email, Inlägg.Titel, Användare.Namn, Användare.Användare_ID from Användare join Inlägg_Användare on Användare.`Användare_ID` = Inlägg_Användare.`Användare_ID` join Inlägg on Inlägg.`Inlägg_ID` = Inlägg_Användare.`Inlägg_ID` join Notifikation on Användare.`Användare_ID` = Notifikation.`Användare_ID` where Inlägg.`Inlägg_ID` = " +  postID + " and Notifiering = 'J'";
+                // fånga kommentarsförfattarna
+                String commentatorsPost = "select Distinct Användare.Email, Inlägg.Titel, Användare.Namn, Kommentarer.`Användare_ID` from Användare join Kommentarer on Kommentarer.`Användare_ID` = Användare.`Användare_ID` join Inlägg on Inlägg.`Inlägg_ID` = Kommentarer.`Inlägg_ID` where Kommentarer.`Inlägg_ID` = " + postID;
+                // select Distinct Användare.Email, Inlägg.Titel, Användare.Namn, Kommentarer.`Användare_ID` from Användare join Kommentarer on Kommentarer.`Användare_ID` = Användare.`Användare_ID` join Inlägg on Inlägg.`Inlägg_ID` = Kommentarer.`Inlägg_ID` join Notifikation on Användare.`Användare_ID` = Notifikation.`Användare_ID` where Inlägg.`Inlägg_ID` = 110 and Notifiering = 'J';
+
+                userInfo1 = st.executeQuery(creatorPost);
+
+            
+            if (userInfo1.next()) {
+             
+//                int sk = userInfo1.getInt(2);
+//                System.out.println(sk);
+
+                creatorOfPost = userInfo1.getString("Inlägg.Titel");
+                emailOfCreator = userInfo1.getString("Användare.Email");
+                   System.out.println(creatorPost);
+                
+                int i = 0;
+                
+                userInfo2 = st.executeQuery(commentatorsPost);
+                while (userInfo2.next()) {                    
+                    
+                    System.out.println("hej");
+                    commentList.add(userInfo2.getString("Användare.Email"));
+                    i++;
+                    
+                    System.out.println("det funkar");
+                }
+                
+                if (!commentList.contains(emailOfCreator)) {
+                    
+                    commentList.add(emailOfCreator);
+                    System.out.println("inlagd");
+     
+                    
+                }
+                
+                else {
+                    
+                    System.out.println("finns redan");
+                    
+                }
+                
+                send_email_gmail seg = new send_email_gmail();
+                seg.mailComments(creatorOfPost, commentList);
+                 
+                
+                
+            }
+            
+            else {
+                
+                System.out.println("kontakta Kai!");
+                
+            }
+  
+            
+                
+            } catch (Exception e) {
+
+                System.out.println("termination");
+            }
         
     }
     
--- CommunicationTool/src/mail/send_email_gmail.java
+++ CommunicationTool/src/mail/send_email_gmail.java
@@ -5,6 +5,8 @@ package mail;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.*; 
 import javax.mail.*; 
 import javax.mail.PasswordAuthentication;
@@ -48,6 +50,53 @@ public class send_email_gmail {
 			InternetAddress.parse(arrayEmail.get(i)));
 			message.setSubject("Titel innehåll");    
 			message.setText("Hej! " + arrayName.get(i) + " En ny post har med titel lagts upp!!");   
+                        
+                        //message.setDisposition("Inlägget handlar om rödluvan och hur hon influerat lärarna på informatik programmet!");
+                    
+			Transport.send(message);
+			System.out.println("Done");                                                                       
+                   }
+                   
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+         
+     }
+    
+    public void mailComments(String title, ArrayList<String> emailCommentator) {
+         //
+         
+         final String username = "InformatikOrebro@gmail.com";
+		final String password = "Jonas991";
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		Session session = Session.getInstance(props,
+		  
+                        new javax.mail.Authenticator() {
+			
+                      protected PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(username, password);
+			}
+		  });
+                
+ 
+		try {
+                    
+                
+                    
+                    //for loop
+                   for(int i = 0; i< emailCommentator.size(); i++)  {
+                             
+                        Message message = new MimeMessage(session);
+			//message.setFrom(new InternetAddress("fromemail@gmail.com"));
+                        message.setFrom(new InternetAddress(emailCommentator.get(i)));                        
+			message.setRecipients(Message.RecipientType.TO,
+			InternetAddress.parse(emailCommentator.get(i)));
+			message.setSubject("Ny kommentar");    
+			message.setText("Hej! " + " En ny kommentar har lagts på tråden " + "'" + title + "'");   
                         
                         //message.setDisposition("Inlägget handlar om rödluvan och hur hon influerat lärarna på informatik programmet!");
                     
